9db84de8b7ddd7fa3e245765455e55efcf7a54ac
hualiu01 <hualiu678@gmail.com>
2024-10-05 12:44:44 -0400

    download top 250

--- clients/python/src/common/os_utils.py
+++ clients/python/src/common/os_utils.py
@@ -1,6 +1,9 @@
 import os
-from typing import Any
+import glob
+from typing import Any,Optional
+import logging 
 
+logger = logging.getLogger(__name__)
 
 def get_required_env_var(name: str) -> Any:
     val = os.getenv(name, "not set")
@@ -12,3 +15,14 @@ def get_required_env_var(name: str) -> Any:
 def file_in_project_data_dir(file_name: str):
     data_dir = get_required_env_var("OMDB_LOCAL_DATA_DIR")
     return f"{data_dir}/{file_name}"
+
+def remove_all_file_with_suffix_in_dir(dir:str, filter_file_suffix:str="json"):
+
+    # Find all JSON files in the folder
+    target_suffix = glob.glob(os.path.join(dir, f'*.{filter_file_suffix}'))
+
+    # Remove each JSON file
+    for t_file in target_suffix:
+        logger.info(f"removing {t_file}...")
+        os.remove(t_file)
+        print(f"Deleted: {t_file}")
--- clients/python/src/data_import/imdb_metadata.py
+++ clients/python/src/data_import/imdb_metadata.py
@@ -1,6 +1,11 @@
 from common.http_utils import get_req
-from common.os_utils import get_required_env_var
+from common.os_utils import get_required_env_var, file_in_project_data_dir, remove_all_file_with_suffix_in_dir
 
+import logging 
+import json
+from os.path import isfile
+
+logger = logging.getLogger(__name__)
 
 class ImdbMetadata:
     def __init__(self):
@@ -16,3 +21,37 @@ class ImdbMetadata:
         return get_req(
             self.omdb_api, {"t": imdb_movie_title, "apikey": self.omdb_api_key}
         )
+    
+    def download_data(url_title):
+        tokens = url_title.strip().split(",")
+        url = tokens[0]
+        title = ",".join(tokens[1:])
+        concat_title = "_".join(title.lower().split(" "))
+        
+        save_to = file_in_project_data_dir(f"raw/{concat_title}.json")
+        if isfile(save_to):
+            return
+        resp_json = imdb_data_importer.get_movie_by_id(url.split("/")[-2])
+        
+        with open(save_to, "w", encoding="utf-8") as f:
+            logger.info(f"saving to {save_to}...")
+            json.dump(resp_json, f, indent=4)
+
+
+    
+if __name__ == "__main__":
+    imdb = ImdbMetadata()
+    imdb_data_importer = ImdbMetadata()
+
+    conf_redownload_all = True
+
+    if conf_redownload_all:
+        remove_all_file_with_suffix_in_dir(file_in_project_data_dir("raw"), filter_file_suffix="json")
+    
+    with open(file_in_project_data_dir("out/IMDB_Top_250_Movies.txt"), 'r',
+              encoding='utf-8') as f:
+        cn = 0
+        for line in f.readlines():
+            ImdbMetadata.download_data(url_title=line)
+            cn += 1
+            logger.info(f"{cn} done")
